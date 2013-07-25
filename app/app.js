753a5cb84b8c54ff5e24f8fda1a0fad81db636d2
John <contact@jadami.com>
2013-07-25 22:26:08 +1000

    Add MVC organization, how cool is that?

--- app/app.js
+++ app/app.js
@@ -1,24 +1,22 @@
 var express = require('express')
   , stylus = require('stylus')
-  , nib = require('nib')
+  , nib = require('nib');
 
-var app = express()
+var app = module.exports = express()
+
 function compile(str, path) {
   return stylus(str)
     .set('filename', path)
     .use(nib())
 }
+
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
-app.use(express.logger('dev'))
+app.use(express.logger('dev')) /*TODO: get this var from the makefile */
 app.use(stylus.middleware( { src: __dirname + '/public' , compile: compile } ))
 app.use(express.static(__dirname + '/public'))
 
-
-
-app.get('/', function (req, res) {
-  res.render('index', { title : 'Home' })
-})
+require('./lib/boot')(app, { verbose: !module.parent });
 
 console.log('Listening to localhost:4000')
 app.listen(4000)
