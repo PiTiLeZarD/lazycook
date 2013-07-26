72928d2c72172ec8eec1e83825327224d678c30d
John <contact@jadami.com>
2013-07-26 19:22:54 +1000

    update tests from tuto, nothing tests DB yet

--- app/test/unit/controllersSpec.js
+++ app/test/unit/controllersSpec.js
@@ -3,13 +3,21 @@ describe('PhoneCat controllers', function() {
   describe('PhoneListCtrl', function(){
     var scope, ctrl, $httpBackend;
 
-    beforeEach(function() {
-      scope = {};
-      ctrl = new PhoneListCtrl(scope);
-    });
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('/phones').
+          respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+ 
+      scope = $rootScope.$new();
+      ctrl = $controller(PhoneListCtrl, {$scope: scope});
+    }));
 
-    it('should create "phones" model with 3 phones', function() {
-      expect(scope.phones.length).toBe(3);
+    it('should create "phones" model with 2 phones fetched from xhr', function() {
+      expect(scope.phones).toBeUndefined();
+      $httpBackend.flush();
+     
+      expect(scope.phones).toEqual([{name: 'Nexus S'},
+                                   {name: 'Motorola DROID'}]);
     });
 
     it('should have a DB Version', function() {
