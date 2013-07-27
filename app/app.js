bf8ef112c0665ac458e521cbe3c664a1fc9ea534
John <contact@jadami.com>
2013-07-27 18:41:25 +1000

    a bit of config

--- app/app.js
+++ app/app.js
@@ -6,27 +6,31 @@ var express = require('express')
 var app = module.exports = express();
 
 /* Global configs */
-app.set('port', process.env.PORT);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.configure( function (){
+  app.set('port', process.env.PORT);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
 
-/* Middlewares */
-app.use(express.logger(app.get('env')));
-app.use(stylus.middleware( { 
-    src: __dirname + '/public' 
-  , compile: function (str, path) { 
-    return stylus(str).set('filename', path).use(nib());
-  } 
-} ));
-app.use(express.static(__dirname + '/public'));
+  app.use(express.logger(app.get('env')));
+  app.use(express.favicon());
+  app.use(stylus.middleware( { 
+      src: __dirname + '/public' 
+    , compile: function (str, path) { 
+      return stylus(str).set('filename', path).use(nib());
+    } 
+  } ));
+  app.use(express.static(__dirname + '/public'));
+});
 
-if (app.get('env') === 'dev') {
-  app.use(express.errorHandler());
-}
+app.configure( 'dev', function (){
+  app.use( express.errorHandler({ dumpExceptions : true, showStack : true }));
+});
+app.configure( 'prod', function (){
+  app.use( express.errorHandler());
+});
 
 /* Initialize DB */
 var conn = db.connect('localhost', parseInt(app.get('port')) + 1);
-
 conn.once('open', function callback () {
   console.log('DB connection successful');
 
