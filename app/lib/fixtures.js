bca0761ebc2cf3df42498b5116d7a7803f189be5
John <contact@jadami.com>
2013-07-28 22:47:17 +1000

    fixtures for some random users

--- app/lib/fixtures.js
+++ app/lib/fixtures.js
@@ -1,8 +1,32 @@
 
 var db = require('./db');
 
-module.exports.User = function(callback) {
-  
-  callback();
+module.exports.User = function(next) {
+
+  new db.User({
+      'login'   : 'admin'
+    , 'password': 'admin'
+    , 'email'   : 'admin-lazycook@mailinator.com'
+    , 'role'    : 'admin'
+  }).save(function(err) {
+    if (err) console.log('Error while saving admin', err);
+
+    var nb_done = 0
+      , users = ['user1', 'user2', 'user3', 'user4'];
+
+    users.forEach( function( username ) {
+      new db.User({
+          'login'   : username
+        , 'password': username
+        , 'email'   : username + '-lazycook@mailinator.com'
+        , 'role'    : 'user'
+      }).save(function(err) {
+        if (err) console.log('Error while saving user', err);
+
+        nb_done += 1;
+        if (nb_done == users.length) next();
+      });
+    });
+  });
   
 };
